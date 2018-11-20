5a61675322ef46f27a18921ceb4ee3957972c36f
a81317 <36645389+a81317@users.noreply.github.com>
2018-11-20 22:30:08 +0000

    Povoada tabela Recursos

--- QuintaScriptPovoamento.sql
+++ QuintaScriptPovoamento.sql
@@ -9,6 +9,20 @@ INSERT INTO animal
         (3, 'Galinha', 3, '2013/01/20', '2021/03/13'),
         (4, 'Cabra', 50, '2010/04/11', '2024/05/23');
 
-TRUNCATE TABLE animal;
-        
-SELECT * FROM animal;
+
+INSERT INTO recurso
+		(ID, Tipo, Fornecedor, Preco, Stock)
+        VALUES
+        (1, 'Água', 'Câmara Municipal', 0.01, 100000),
+        (2, 'Adubo', 'ADP Fertilizantes', 0.50, 1000),
+        (3, 'Herbicida', 'Bayer', 20, 124),
+        (4, 'Farinha para galinhas', 'Loja do Gusto', 0.30, 700),
+        (5, 'Farinha para vacas', 'Utilagro', 0.35, 1820);
+
+-- Cuidado, aldrabice 
+SET foreign_key_checks = 0;
+TRUNCATE  TABLE animal;
+SET foreign_key_checks = 1;
+
+
+SELECT * FROM recurso;
