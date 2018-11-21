73987e46f5290eb1fedc4d3c33197a6cfa338779
Luís Correia <luisbcorreia98@gmail.com>
2018-11-21 18:40:27 +0000

    Alterado views

--- Views.sql
+++ Views.sql
@@ -1,7 +1,10 @@
 USE quinta;
 
 -- Nome e Horário de funcionários
+DROP VIEW Horarios;
 CREATE VIEW Horarios AS
-	SELECT F.Nome, F.Horário FROM Funcionário AS F;
+	SELECT F.Nome, F.Horário, FP.Plantação_ID, FA.Animal_ID FROM Funcionário AS F
+		LEFT JOIN FuncionárioPlantação AS FP ON F.Número = FP.Funcionário_Número
+		LEFT JOIN FuncionárioAnimal AS FA ON F.Número = FA.Funcionário_Número;
 
 -- SELECT * FROM Horarios;
