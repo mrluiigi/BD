c4d1d9bfddc29077f3449d2e85dd74a4c59904fc
Luís Correia <luisbcorreia98@gmail.com>
2018-11-21 14:55:37 +0000

    Modificado Interrogações.sql

--- Interrogações.sql
+++ Interrogações.sql
@@ -1,10 +1,13 @@
 USE quinta;
 
--- verifica que animais expiram a vacina dentro de um mês
+-- Verifica que animais expiram a vacina dentro de um mês
 SELECT * FROM animal WHERE ValidadeVacina <= (DATE(NOW()) + INTERVAL 1 MONTH);
 
--- altera validade da vacina +2 anos
+
+-- Altera validade da vacina +2 anos
+DROP PROCEDURE IF EXISTS alteraValidadeVacina;
 DELIMITER //
+
 CREATE PROCEDURE alteraValidadeVacina(idAnimal INT)
 BEGIN
 UPDATE animal 
@@ -13,8 +16,12 @@ UPDATE animal
 END //
 DELIMITER ;
 
--- adiciona ao stock de um certo recurso uma quantidade
+
+
+-- Adiciona ao stock de um certo recurso uma quantidade
+DROP PROCEDURE IF EXISTS adicionaStockRecurso;
 DELIMITER //
+
 CREATE PROCEDURE adicionaStockRecurso(idRecurso INT, quantidade INT)
 BEGIN
 UPDATE recurso
@@ -23,16 +30,36 @@ UPDATE recurso
 END//
 DELIMITER ;
 
--- Pantação a colher no mês indicado
+
+-- Plantação a colher no mês indicado
+DROP PROCEDURE IF EXISTS colheitaMes;
 DELIMITER //
+
 CREATE PROCEDURE colheitaMes(mes int)
 BEGIN
 	SELECT * FROM plantação 
 		WHERE MesColheita = mes;
 END//
 DELIMITER //;
+-- CALL colheitaMes(6);
 
-CALL colheitaMes(6);
+
+-- Quantidade em Stock do recurso X
+DROP FUNCTION IF EXISTS quantidadeRecurso;
+DELIMITER %%
+
+CREATE FUNCTION quantidadeRecurso(id INT) RETURNS INT
+READS SQL DATA
+	BEGIN 
+		DECLARE quantidade INT;
+        
+        SELECT R.Stock INTO quantidade FROM Recurso AS R
+			WHERE R.ID = id;
+		RETURN quantidade;
+        
+	END %%
+DELIMITER ;
+-- SELECT quantidadeRecurso(4);
 
 
 -- Quantos espécies de animais existem na quinta 
