01215cb2ebb5b3af657b6b1a90ecdf35cc74e5b7
Luís Correia <luisbcorreia98@gmail.com>
2018-11-21 19:06:16 +0000

    Alterado Permissoes

--- Permissions.sql
+++ Permissions.sql
@@ -1,9 +1,16 @@
-CREATE USER 'gestor'@'localhost' IDENTIFIED BY 'aminhapass';
+-- Gestor
+CREATE USER 'gestor'@'localhost' IDENTIFIED BY 'manel42';
+-- DROP USER 'gestor'@'localhost';
 
+-- Permissões do gestor
 GRANT DELETE, INSERT, SELECT, UPDATE ON quinta.* TO 'gestor'@'localhost';
 
-CREATE USER 'funcionario'@'localhost' IDENTIFIED BY 'aminhapass';
 
+-- Funcionário
+CREATE USER 'funcionario'@'localhost' IDENTIFIED BY 'aminhapass';
+-- DROP USER 'funcionario'@'localhost';
+
+-- Permissões do Funcionário
 GRANT DELETE, INSERT, SELECT, UPDATE ON quinta.Animal TO 'funcionario'@'localhost';
 
 GRANT DELETE, INSERT, SELECT, UPDATE ON quinta.ProdutoAnimal TO 'funcionario'@'localhost';
@@ -12,6 +19,9 @@ GRANT DELETE, INSERT, SELECT, UPDATE ON quinta.ProdutoVegetal TO 'funcionario'@'
 
 GRANT DELETE, INSERT, SELECT, UPDATE ON quinta.ProdutoVegetal TO 'funcionario'@'localhost';
 
-GRANT EXECUTE ON PROCEDURE quinta.qualFuncionario TO 'funcionario'@'localhost';
+GRANT EXECUTE ON quinta.* TO 'funcionario'@'localhost';
 
-SELECT * FROM mysql.user;
+
+
+
+-- SELECT * FROM mysql.user;
