b73e5fe5869819d801934f25206961c73173e44f
Angelo Sousa <angeloandre1998@gmail.com>
2018-11-21 13:52:42 +0000

    Adiciona stock , Validade Vacina

--- Interrogações.sql
+++ Interrogações.sql
@@ -1,5 +1,28 @@
 USE quinta;
 
+-- verifica que animais expiram a vacina dentro de um mês
+SELECT * FROM animal WHERE ValidadeVacina <= (DATE(NOW()) + INTERVAL 1 MONTH);
+
+-- altera validade da vacina +2 anos
+DELIMITER //
+CREATE PROCEDURE alteraValidadeVacina(idAnimal INT)
+BEGIN
+UPDATE animal 
+	SET ValidadeVacina = (ValidadeVacina + INTERVAL 2 YEAR)
+    WHERE animal.ID = idAnimal;
+END //
+DELIMITER ;
+
+-- adiciona ao stock de um certo recurso uma quantidade
+DELIMITER //
+CREATE PROCEDURE adicionaStockRecurso(idRecurso INT, quantidade INT)
+BEGIN
+UPDATE recurso
+	SET Stock = Stock + quantidade
+    where recurso.ID = idRecurso;
+END//
+DELIMITER ;
+
 -- Pantação a colher no mês indicado
 DELIMITER //
 CREATE PROCEDURE colheitaMes(mes int)
@@ -14,3 +37,4 @@ CALL colheitaMes(6);
 
 -- Quantos espécies de animais existem na quinta 
 SELECT COUNT(DISTINCT Espécie) AS TotalEspécies FROM Animal;
+
