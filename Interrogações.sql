f5a1115d1f6133f03315a6a9b5031b6c2b3b308e
Luís Correia <luisbcorreia98@gmail.com>
2018-11-24 13:04:38 +0000

    updated Interrogações

--- Interrogações.sql
+++ Interrogações.sql
@@ -4,10 +4,14 @@ USE quinta;
 SELECT * FROM animal WHERE ValidadeVacina <= (DATE(NOW()) + INTERVAL 1 MONTH);
 
 
+-- Quantas espécies de animais existem na quinta 
+SELECT COUNT(DISTINCT Espécie) AS TotalEspécies FROM Animal;
+
+
 -- Altera validade da vacina +2 anos
 DROP PROCEDURE IF EXISTS alteraValidadeVacina;
-DELIMITER //
 
+DELIMITER //
 CREATE PROCEDURE alteraValidadeVacina(idAnimal INT)
 BEGIN
 UPDATE animal 
@@ -20,64 +24,74 @@ DELIMITER ;
 
 -- Adiciona ao stock de um certo recurso uma quantidade
 DROP PROCEDURE IF EXISTS adicionaStockRecurso;
-DELIMITER //
 
+DELIMITER //
 CREATE PROCEDURE adicionaStockRecurso(idRecurso INT, quantidade INT)
 BEGIN
 UPDATE recurso
 	SET Stock = Stock + quantidade
-    where recurso.ID = idRecurso;
+	WHERE recurso.ID = idRecurso;
 END//
 DELIMITER ;
 
--- remove stock do recurso animal e regista que animal consumiu e que quantidade consumiu e a data do ultimo consumo do mesmo animal
+
+-- Remove stock do recurso dado e regista que animal consumiu, que quantidade consumiu e a data do último consumo do mesmo animal
+DROP PROCEDURE IF EXISTS removeStockRecursoAnimal;
+
 DELIMITER //
 CREATE PROCEDURE removeStockRecursoAnimal(idRecurso INT, quantidade INT, idAnimal INT)
 BEGIN
-		if(quantidadeRecurso(idRecurso) > quantidade) then
-		UPDATE animalrecurso
+	IF(quantidadeRecurso(idRecurso) > quantidade) 
+		THEN
+			UPDATE animalrecurso
 				SET QuantidadeConsumida = QuantidadeConsumida + quantidade,
-                DataUltimoConsumo = NOW()
-                WHERE Recurso_ID = idRecurso AND Animal_ID = idAnimal;
-			 UPDATE recurso
+					DataUltimoConsumo = NOW()
+				WHERE Recurso_ID = idRecurso AND Animal_ID = idAnimal;
+			UPDATE recurso
 				SET Stock = Stock - quantidade
 				WHERE ID = idRecurso;
-	end if;
+	END IF;
 END //
--- remove stock do recurso plantação e regista que plantação consumiu e que quantidade e a data do ultimo consumo da plantação
+
+
+-- Remove stock do recurso dado e regista que plantação consumiu, que quantidade e a data do último consumo da plantação
+DROP PROCEDURE IF EXISTS removeStockRecursoPlantacao;
+
 DELIMITER //
 CREATE PROCEDURE removeStockRecursoPlantacao(idRecurso INT, quantidade INT, idPlantacao INT)
 BEGIN
-		if(quantidadeRecurso(idRecurso) > quantidade) then
-		UPDATE plantaçãorecurso
+	IF(quantidadeRecurso(idRecurso) > quantidade) 
+		THEN
+			UPDATE plantaçãorecurso
 				SET QuantidadeConsumida = QuantidadeConsumida + quantidade,
-                DataUltimoConsumo = NOW()
-                WHERE Recurso_ID = idRecurso AND Plantação_ID = idPlantacao;
-			 UPDATE recurso
+					DataUltimoConsumo = NOW()
+				WHERE Recurso_ID = idRecurso AND Plantação_ID = idPlantacao;
+			UPDATE recurso
 				SET Stock = Stock - quantidade
 				WHERE ID = idRecurso;
-	end if;
+	END IF;
 END //
 DELIMITER ;
 
-DELIMITER ;
+
 -- Plantação a colher no mês indicado
 DROP PROCEDURE IF EXISTS colheitaMes;
-DELIMITER //
 
-CREATE PROCEDURE colheitaMes(mes int)
+DELIMITER //
+CREATE PROCEDURE colheitaMes(mes INT)
 BEGIN
-	SELECT * FROM plantação 
-		WHERE MesColheita = mes;
+	SELECT * FROM plantação AS P 
+		WHERE P.MesColheita = mes;
 END//
-DELIMITER //;
+DELIMITER ;
+
 -- CALL colheitaMes(6);
 
 
 -- Quantidade em Stock do recurso X
 DROP FUNCTION IF EXISTS quantidadeRecurso;
-DELIMITER %%
 
+DELIMITER %%
 CREATE FUNCTION quantidadeRecurso(id INT) RETURNS INT
 READS SQL DATA
 	BEGIN 
@@ -92,13 +106,12 @@ DELIMITER ;
 -- SELECT quantidadeRecurso(4);
 
 
--- Quantos espécies de animais existem na quinta 
-SELECT COUNT(DISTINCT Espécie) AS TotalEspécies FROM Animal;
 
 
 
 -- Que funcionario cuidou do Animal X
 DROP PROCEDURE IF EXISTS qualFuncionario
+
 DELIMITER %%
 CREATE PROCEDURE qualFuncionario(idAnimal INT)
 	BEGIN
@@ -112,181 +125,200 @@ DELIMITER ;
 -- CALL qualFuncionario(3);
 
 
-
-
-
-
+-- Verifica se um determinado animal existe. Retorna 1 se existe e 0 caso contrário.
 DELIMITER %%
-
 CREATE FUNCTION existeAnimal(id INT) RETURNS INT
 READS SQL DATA
 	BEGIN 
-    DECLARE existe INT;
-	SELECT COUNT(A.id) INTO existe FROM Animal AS A
-			WHERE A.ID = id;
-	RETURN existe;
+		DECLARE existe INT;
+		SELECT COUNT(A.id) INTO existe FROM Animal AS A
+				WHERE A.ID = id;
+		RETURN existe;
 	END %%
 DELIMITER ;
 
 
+
 -- Soma das vendas num intervalo de tempo 
-delimiter //
+DELIMITER //
 CREATE PROCEDURE valorVendasIntervalo (inicio DATE, fim DATE)
 	BEGIN 
 		SELECT SUM(Valor) FROM Encomenda 
-			WHERE (data BETWEEN inicio  AND fim);
+			WHERE (data BETWEEN inicio AND fim);
 	END//
-delimiter ;
+DELIMITER ;
 
--- valor das vendas totais de um cliente
+
+-- Valor das vendas totais de um cliente
+
 DELIMITER %%
 CREATE FUNCTION totalVendasCliente(id INT) RETURNS INT
 READS SQL DATA
 	BEGIN
 		DECLARE total INT;
+        
 		SELECT Valor INTO total FROM cliente
-		INNER JOIN encomenda ON encomenda.cliente = cliente.NIF AND cliente.NIF = id;
+		INNER JOIN encomenda ON encomenda.Cliente = cliente.NIF 
+			WHERE cliente.NIF = id;
+        
         RETURN total;
-	END%%
+	END %%
 DELIMITER ;
+-- SELECT totalVendasCliente(2);
 
--- produtos do animal x
+
+-- Produtos do animal x
 
 DELIMITER //
-CREATE PROCEDURE produtosDoAnimal(id int)
-BEGIN
-	select * from produtoanimal where Animal_ID = id;
-END//
+CREATE PROCEDURE produtosDoAnimal(id INT)
+	BEGIN
+		SELECT * FROM produtoanimal WHERE Animal_ID = id;
+	END//
 DELIMITER ;
+-- CALL produtosDoAnimal(2);
 
 
-
+-- Produtos animais que tem validade abaixo da data indicada
 DROP PROCEDURE IF EXISTS passouValidadeAnimal;
+
 DELIMITER //
 CREATE PROCEDURE passouValidadeAnimal(dataA DATE)
-BEGIN
+	BEGIN
         SELECT * FROM produtoanimal 
-                WHERE Validade <= dataA;
-END//
-DELIMITER //;
+			WHERE Validade <= dataA;
+	END //
+DELIMITER ;
+-- CALL passouValidadeAnimal('2018-11-20');
 
-DROP PROCEDURE IF EXISTS produtovegetal;
+
+-- Produtos vegetais que tem data de validade abaixo da dada.
+DROP PROCEDURE IF EXISTS passouValidadePlanta;
+
 DELIMITER //
 CREATE PROCEDURE passouValidadePlanta(dataA DATE)
-BEGIN
+	BEGIN
         SELECT * FROM produtovegetal
-                WHERE Validade <= dataA;
-END//
-DELIMITER //;
+			WHERE Validade <= dataA;
+	END//
+DELIMITER ;
 
 
--- Lucro gerado por um animal/plantação (preço dos produtos - custo dos recursos) (num intervalo de tempo?)
-
--- Custo dos recursos: tabela do animal com o animalrecurso para saber quantidade consumida e depois animalrecurso com recurso para saber o preço
--- Associar produtoanimal com encomenda para ver o lucro que esse animal gerou
-
+-- Recursos consumidos pelo animal dado
 DROP PROCEDURE IF EXISTS recursosConsumidosAnimal;
 
 DELIMITER //
 CREATE PROCEDURE recursosConsumidosAnimal(idAnimal INT)
 BEGIN
         SELECT A.ID, A.Espécie, R.Tipo, AR.QuantidadeConsumida FROM animal AS A
-                INNER JOIN animalrecurso AS AR ON AR.Animal_ID = A.ID
-        INNER JOIN recurso AS R ON R.ID = AR.Recurso_ID
+			INNER JOIN animalrecurso AS AR ON AR.Animal_ID = A.ID
+			INNER JOIN recurso AS R ON R.ID = AR.Recurso_ID
                 WHERE A.ID = idAnimal;
 END //
-DELIMITER //;
+DELIMITER ;
 
 CALL recursosConsumidosAnimal(1);
 
 
+-- Calcula o custo associado à alimentação de um animal
+
 DELIMITER %%
-CREATE FUNCTION calculaCustoConsumidoAnimal(idAnimal INT) RETURNS int
+CREATE FUNCTION calculaCustoConsumidoAnimal(idAnimal INT) RETURNS INT
 READS SQL DATA
 	BEGIN 
 		DECLARE total INT;
+        
         SELECT SUM(R.Preco * AR.QuantidadeConsumida) INTO total FROM animal AS A
             INNER JOIN animalrecurso AS AR ON AR.Animal_ID = A.ID
 			INNER JOIN recurso AS R ON R.ID = AR.Recurso_ID
                 WHERE A.ID = idAnimal;
                 
-			RETURN total;
+		RETURN total;
 	END %%
-DELIMITER %%;
+DELIMITER ;
+-- SELECT calculaCustoConsumidoAnimal(3)
+
+
+-- Calcula o valor de todas as vendas dos produtos provindos de certo animal
+DROP Function calculaVendaProdutosAnimal;
 
 DELIMITER %%
 CREATE FUNCTION calculaVendaProdutosAnimal(idAnimal INT) RETURNS INT
 READS SQL DATA
 	BEGIN 
 		DECLARE total INT;
-        SELECT SUM(PA.Preco * PAE.Quantidade) INTO total FROM animal AS A
-			INNER JOIN produtoanimal AS PA ON PA.Animal_ID = A.ID
-			INNER JOIN produtoanimalencomenda AS PAE ON PAE.ProdutoAnimal_ID = PA.ID
-				WHERE A.ID = idAnimal;
+       
+		SELECT SUM(PA.Preco * PAE.Quantidade) INTO total FROM ProdutoAnimal AS PA
+			INNER JOIN ProdutoAnimalEncomenda AS PAE ON PAE.ProdutoAnimal_ID = PA.ID
+				WHERE PA.Animal_ID = idAnimal;
                 
-			RETURN total;
+		RETURN total;
 	END %%
-DELIMITER %%;
+DELIMITER ;
+-- SELECT calculaVendaProdutosAnimal(8);
 
+
+-- Calcula o lucro que um certo animal forneceu
+
+DROP PROCEDURE calculaLucro;
 DELIMITER //
 CREATE PROCEDURE calculaLucro(idAnimal INT)
-BEGIN
-    SELECT (calculaVendaProdutosAnimal(idAnimal) - calculaCustoConsumidoAnimal(idAnimal));
-END //
-DELIMITER //;
+	BEGIN
+		SELECT (calculaVendaProdutosAnimal(idAnimal) - calculaCustoConsumidoAnimal(idAnimal));
+	END //
+DELIMITER ;
+
+CALL calculaLucro(8);
+
 
 -- Funcionários da Plantação X
 DELIMITER //
 CREATE PROCEDURE funcionariosDaPlantacao(idPlantação INT)
 BEGIN
-	SELECT * FROM funcionário 
-		INNER JOIN funcionárioplantação AS FP ON FP.Funcionário_Número = funcionário.Número
-		WHERE Plantação_ID = idPlantação;
+	SELECT * FROM funcionário AS F
+		INNER JOIN funcionárioplantação AS FP ON FP.Funcionário_Número = F.Número
+			WHERE FP.Plantação_ID = idPlantação;
 END //
 DELIMITER ;
+-- CALL funcionariosDaPlantacao(3);
 
 
 -- Número de animais da espécie X
-
 DELIMITER //
 CREATE PROCEDURE animalEspecie(especie varchar(45))
-BEGIN
-    SELECT COUNT(*) FROM animal
-		WHERE animal.Espécie = especie AND animal.Vida = 'V';
-END //
-DELIMITER //;
+	BEGIN
+		SELECT COUNT(*) FROM animal
+			WHERE animal.Espécie = especie AND animal.Vida = 'V';
+	END //
+DELIMITER ;
 
 CALL animalEspecie('Vaca minhota');
 
 
 
 -- Área total das plantações (é preciso atributo para saber se foi colhida)
-
 DELIMITER //
 CREATE PROCEDURE areaPlantacoes()
 BEGIN
     SELECT SUM(Area) FROM plantação;
 END //
-DELIMITER //;
+DELIMITER ;
 
 CALL areaPlantacoes();
 
 
--- Numero de encomendas feitas
-
+-- Número de encomendas feitas
 DELIMITER //
 CREATE PROCEDURE quantasEncomendas()
 BEGIN
     SELECT COUNT(*) FROM encomenda;
 END //
-DELIMITER //;
+DELIMITER ;
 
 CALL quantasEncomendas();
 
 
 
 -- Que espécies consomem o recurso X
-
 DELIMITER //
 CREATE PROCEDURE especiesConsomem(recurso INT)
 BEGIN
@@ -294,6 +326,12 @@ BEGIN
 		INNER JOIN animalrecurso AS AR ON AR.Animal_ID = animal.ID
         WHERE AR.Recurso_ID = recurso;
 END //
-DELIMITER //;
+DELIMITER ;
 
 CALL especiesConsomem(1);
+
+
+-- Lucro gerado por um animal/plantação (preço dos produtos - custo dos recursos) (num intervalo de tempo?)
+
+-- Custo dos recursos: tabela do animal com o animalrecurso para saber quantidade consumida e depois animalrecurso com recurso para saber o preço
+-- Associar produtoanimal com encomenda para ver o lucro que esse animal gerou
